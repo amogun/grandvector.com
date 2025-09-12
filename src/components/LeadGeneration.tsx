a3aad8c4f5d72c97371c2ef627d5c97feb43cff0
raffaellobueno <raffaellobueno@gmail.com>
2025-09-12 22:50:33 +0200

    Fix mobile Spline animation cuts on all 4 sides

--- src/components/LeadGeneration.tsx
+++ src/components/LeadGeneration.tsx
@@ -58,7 +58,7 @@ const LeadGeneration = () => {
           {/* Right Side - Spline Animation */}
           <div className="relative h-[600px] p-8 lg:p-8 p-4">
             <div className="h-full overflow-hidden">
-              <div className="h-[120%] w-[150%] lg:w-[120%] transform -translate-y-[10%] -translate-x-[25%] lg:-translate-x-[10%]" style={{clipPath: 'inset(0 0 10% 0)'}}>
+              <div className="h-[100%] w-[100%] lg:h-[120%] lg:w-[120%] transform translate-y-0 translate-x-0 lg:-translate-y-[10%] lg:-translate-x-[10%]" style={{clipPath: 'inset(0 0 0% 0) lg:inset(0 0 10% 0)'}}>
                 <spline-viewer url="https://prod.spline.design/p7W1gAIKcdk5sjLy/scene.splinecode"></spline-viewer>
               </div>
             </div>
